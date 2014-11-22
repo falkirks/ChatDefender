804b4a6bab08e9a839180a29f34fa7caad55c1f5
Falkirks <bpnoreply@gmail.com>
2014-11-22 15:43:46 -0800

    Initial commit

---
+++ src/falkirks/chatdefender/ChatDefender.php
@@ -0,0 +1,37 @@
+<?php
+namespace falkirks\chatdefender;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class ChatDefender extends PluginBase implements Listener{
+    /** @var  ChatSession[] */
+    public $sessions;
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->s = [];
+    }
+    public function onChat(PlayerChatEvent $event){
+        if(!isset($this->sessions[$event->getPlayer()->getName()])){
+            $this->sessions[$event->getPlayer()->getName()] = new ChatSession($this);
+            $this->sessions[$event->getPlayer()->getName()]->bindToPlayer($event->getPlayer());
+        }
+        if(!$this->sessions[$event->getPlayer()->getName()]->sendMessage($event->getMessage())){
+            $event->setCancelled();
+        }
+    }
+    public function onQuit(PlayerQuitEvent $event){
+        if(isset($this->sessions[$event->getPlayer()->getName()])) unset($this->sessions[$event->getPlayer()->getName()]);
+    }
+    public function onCommandPreProcess(PlayerCommandPreprocessEvent $event){
+        if($this->getConfig()->get("blockme") && isset($this->sessions[$event->getPlayer()->getName()]) && $this->sessions[$event->getPlayer()->getName()]->isBlocked()){
+            $args = explode(" ", $event->getMessage());
+            if($args[0] == "/me"){
+                $event->setCancelled();
+            }
+        }
+    }
+}
---
+++ src/falkirks/chatdefender/ChatSession.php
@@ -0,0 +1,57 @@
+<?php
+namespace falkirks\chatdefender;
+use pocketmine\Player;
+use pocketmine\scheduler\PluginTask;
+
+class ChatSession extends PluginTask{
+    private $state;
+    private $m;
+    private $blockLength;
+    /** @var  Player */
+    private $player;
+    public function bindToPlayer(Player $player){
+        $this->player = $player;
+        $this->blockLength = 1;
+        $this->state = 0;
+    }
+    public function sendMessage($message){
+        if($this->state == 2){
+            $this->player->sendMessage("[ChatDefender] You are currently blocked.");
+            return false;
+        }
+        elseif($this->similarityCheck($message) || $this->rateCheck()){
+            if($this->state == 1){
+                $this->state = 2;
+                $this->player->sendMessage("[ChatDefender] You have been blocked for " . $this->blockLength * $this->getOwner()->getConfig()->get("baseblocklength") . "second(s)");
+                $this->getOwner()->getServer()->getScheduler()->scheduleDelayedTask($this, 20*$this->getOwner()->getConfig()->get("baseblocklength")*$this->blockLength);
+                $this->log++;
+                return false;
+            }
+            else{
+                $this->state = 1;
+                $this->player->sendMessage("[ChatDefender] Looks like you are posting spammy stuff. Continuing to do so will result in a block.");
+                $this->m = array($message, time());
+                return false;
+            }
+        }
+        else{
+            $this->m = array($message, time());
+            return true;
+        }
+    }
+    public function onRun($tick){
+        $this->state = 0;
+        $this->player->sendMessage("[ChatDefender] Your block has been lifted.");
+    }
+    public function similarityCheck($message){
+        if($this->player->hasPermission("chatdefender.exempt.similar")) return false;
+        return ((strlen($message) - similar_text($this->m[0],$message)) <= $this->getOwner()->getConfig()->get("similarity"));
+    }
+    public function rateCheck(){
+        if($this->player->hasPermission("chatdefender.exempt.rate")) return false;
+        return ((time() - $this->m[1]) <= $this->getOwner()->getConfig()->get("ratelimit"));
+    }
+    public function isBlocked(){
+        return $this->state === 2;
+    }
+}
